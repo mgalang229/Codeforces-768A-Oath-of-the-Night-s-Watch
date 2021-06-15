f6f30516d9ec0264e0a0a3b2f63c186ad4c80b4e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-06-15 12:23:31 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	// sort the sequence in non-decreasing order
+	sort(a.begin(), a.end());
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		// if the current element is STRICTLY greater than the smallest element if it's 
+		// STRICLY smaller than the largest element
+		if (a[i] > a[0] && a[i] < a[n - 1]) {
+			cnt++;
+		}
+	}
+	cout << cnt << '\n';
+	return 0;
+}
